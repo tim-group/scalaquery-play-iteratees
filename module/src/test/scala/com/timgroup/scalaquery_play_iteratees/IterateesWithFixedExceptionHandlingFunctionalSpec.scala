23b64ed69ac3f152ecb8be6b659a196b62ec51f5
Marc Siegel <marc.siegel@timgroup.com>
2013-11-14 16:01:10 -0500

    Marc: [module] extract tests from FX app and port to the module

--- module/src/test/scala/com/timgroup/scalaquery_play_iteratees/IterateesWithFixedExceptionHandlingFunctionalSpec.scala
+++ module/src/test/scala/com/timgroup/scalaquery_play_iteratees/IterateesWithFixedExceptionHandlingFunctionalSpec.scala
@@ -13,64 +13,68 @@ class IterateesWithFixedExceptionHandlingFunctionalSpec extends path.FunSpec wit
   val unparseableCallbackPromise: Promise[Option[String]] = Promise.pure(Some("not an Int"))
   val failedCallbackPromise: Promise[Option[String]] = Promise.pure(throw new RuntimeException("boo!"))
 
-  describe("[Demonstrate problem] Pipeline of Enumerator.fromCallback &> Enumeratee |>> Iteratee") {
+  describe("Enumerator.fromCallback") {
 
-    it("fails to return a failed promise containing an exception thrown in Enumerator") {
-      val enumerator = Enumerator.fromCallback(() => failedCallbackPromise)
-      val enumeratee = Enumeratee.map { (s: String) => s.toInt }
-      val iteratee = Iteratee.fold[Int, Int](0) { (sum, n) => sum + n }
-      val eventuallyResult = (enumerator &> enumeratee |>> iteratee).flatMap(_.run)
+    describe("[Demonstrate problem] Pipeline of Enumerator.fromCallback &> Enumeratee |>> Iteratee") {
 
-      // NOTE (2013-10-25, msiegel/broberts): Instead of propagating exception, it times out
-      //val thrown = evaluating { eventuallyResult.await(100).get } must produce [RuntimeException]
-      //thrown.getMessage must equal("boo!")
+      it("fails to return a failed promise containing an exception thrown in Enumerator") {
+        val enumerator = Enumerator.fromCallback(() => failedCallbackPromise)
+        val enumeratee = Enumeratee.map { (s: String) => s.toInt }
+        val iteratee = Iteratee.fold[Int, Int](0) { (sum, n) => sum + n }
+        val eventuallyResult = (enumerator &> enumeratee |>> iteratee).flatMap(_.run)
 
-      // Demonstrate that the following problem exists to motivate our fix:
-      evaluating { eventuallyResult.await(100).get } must produce [TimeoutException]
+        // NOTE (2013-10-25, msiegel/broberts): Instead of propagating exception, it times out
+        //val thrown = evaluating { eventuallyResult.await(100).get } must produce [RuntimeException]
+        //thrown.getMessage must equal("boo!")
+
+        // Demonstrate that the following problem exists to motivate our fix:
+        evaluating { eventuallyResult.await(100).get } must produce [TimeoutException]
+      }
+
     }
 
-  }
+    describe("[Demonstrate problem] Enumerator.fromCallback") {
 
-  describe("[Demonstrate problem] Enumerator.fromCallback") {
+      it("fails to pass along a failed promise containing an exception thrown in callback") {
+        val enumerator = Enumerator.fromCallback(() => failedCallbackPromise)
+        val iteratee = Iteratee.ignore[String]
+        val eventuallyResult = (enumerator |>> iteratee).flatMap(_.run)
 
-    it("fails to pass along a failed promise containing an exception thrown in callback") {
-      val enumerator = Enumerator.fromCallback(() => failedCallbackPromise)
-      val iteratee = Iteratee.ignore[String]
-      val eventuallyResult = (enumerator |>> iteratee).flatMap(_.run)
+        // NOTE (2013-10-25, msiegel/broberts): Instead of propagating exception, it times out
+        //val thrown = evaluating { eventuallyResult.await(100).get } must produce [RuntimeException]
+        //thrown.getMessage must equal("boo!")
 
-      // NOTE (2013-10-25, msiegel/broberts): Instead of propagating exception, it times out
-      //val thrown = evaluating { eventuallyResult.await(100).get } must produce [RuntimeException]
-      //thrown.getMessage must equal("boo!")
+        // Demonstrate that the following problem exists to motivate our fix:
+        evaluating { eventuallyResult.await(100).get } must produce [TimeoutException]
+      }
 
-      // Demonstrate that the following problem exists to motivate our fix:
-      evaluating { eventuallyResult.await(100).get } must produce [TimeoutException]
     }
 
-  }
+    describe("IterateesWithFixedExceptionHandling.Enumerator.fromCallback") {
 
-  describe("IterateesWithFixedExceptionHandling.Enumerator.fromCallback") {
+      it("passes along a failed promise containing an exception thrown in callback") {
+        val enumerator = IterateesWithFixedExceptionHandling.Enumerator.fromCallback(() => failedCallbackPromise)
+        val iteratee = Iteratee.ignore[String]
+        val eventuallyResult = (enumerator |>> iteratee).flatMap(_.run)
 
-    it("passes along a failed promise containing an exception thrown in callback") {
-      val enumerator = IterateesWithFixedExceptionHandling.Enumerator.fromCallback(() => failedCallbackPromise)
-      val iteratee = Iteratee.ignore[String]
-      val eventuallyResult = (enumerator |>> iteratee).flatMap(_.run)
+        val thrown = evaluating { eventuallyResult.await(100).get } must produce [RuntimeException]
+        thrown.getMessage must equal("boo!")
+      }
 
-      val thrown = evaluating { eventuallyResult.await(100).get } must produce [RuntimeException]
-      thrown.getMessage must equal("boo!")
     }
 
-  }
+    describe("Pipeline of fixed Enumerator.fromCallback &> Enumeratee |>> Iteratee") {
 
-  describe("Pipeline of fixed Enumerator.fromCallback &> Enumeratee |>> Iteratee") {
+      it("returns a failed promise containing an exception thrown in Enumerator") {
+        val enumerator = IterateesWithFixedExceptionHandling.Enumerator.fromCallback(() => failedCallbackPromise)
+        val enumeratee = Enumeratee.map { (s: String) => s.toInt }
+        val iteratee = Iteratee.fold[Int, Int](0) { (sum, n) => sum + n }
+        val eventuallyResult = (enumerator &> enumeratee |>> iteratee).flatMap(_.run)
 
-    it("returns a failed promise containing an exception thrown in Enumerator") {
-      val enumerator = IterateesWithFixedExceptionHandling.Enumerator.fromCallback(() => failedCallbackPromise)
-      val enumeratee = Enumeratee.map { (s: String) => s.toInt }
-      val iteratee = Iteratee.fold[Int, Int](0) { (sum, n) => sum + n }
-      val eventuallyResult = (enumerator &> enumeratee |>> iteratee).flatMap(_.run)
+        val thrown = evaluating { eventuallyResult.await(100).get } must produce [RuntimeException]
+        thrown.getMessage must equal("boo!")
+      }
 
-      val thrown = evaluating { eventuallyResult.await(100).get } must produce [RuntimeException]
-      thrown.getMessage must equal("boo!")
     }
 
   }

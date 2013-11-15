d756bbfccc3be322be5b48474ae7a5f5d67e7378
Marc Siegel <marc.siegel@timgroup.com>
2013-11-15 16:23:13 -0500

    Marc: [sample] allow up to 2 sec for comet to load in sample app test

--- sample/test/IntegrationSpec.scala
+++ sample/test/IntegrationSpec.scala
@@ -1,5 +1,7 @@
 package test
 
+import java.util.concurrent.TimeUnit
+
 import org.specs2.mutable._
 
 import play.api.test._
@@ -14,6 +16,7 @@ class IntegrationSpec extends Specification {
 
         // verify that comet handler received records in chunks
         browser.goTo("http://localhost:3333/")
+        browser.await().atMost(2, TimeUnit.SECONDS).until("#items-list ul li").isPresent()
         browser.pageSource must contain("""{"id":1,"name":"Alpha"}""")
         browser.pageSource must contain("""{"id":2,"name":"Beta"}""")
         browser.pageSource must contain("""{"id":3,"name":"Gamma"}""")
